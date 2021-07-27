bc6ddc76a2f70bca1aa0ac870eea9f4c4b0e19d7
Okera Murray <murrayokera@gmail.com>
2021-07-27 16:58:39 -0400

    Initial Commit

---
+++ Prework(initial tip calculator)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prework(initial tip calculator)
+//
+//  Created by Okera Murray on 7/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
